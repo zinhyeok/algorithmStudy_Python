e52e220ecb1dcf2646a2cea166138903b74c2bd4
zinhyeok <david9pjh@gmail.com>
2023-04-02 00:23:37 +0900

    0401:[1835.py O]

---
+++ deque/1835.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+
+def ans():
+    N = int(input())
+    deq = deque()
+    for i in range(N, 0, -1):
+        deq.appendleft(i)
+        for j in range(i):
+            tmp = deq.pop()
+            deq.appendleft(tmp)
+
+    for i in deq:
+        print(i, end=" ")
+
+
+
+ans()
